3c105f4ce4de474422a873090652391cef55ff5c
Cherrerotinoco <c.herrerotinoco@gmail.com>
2021-09-13 17:02:29 +0200

    Complete steps 5 and 6

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,31 +1,71 @@
 import React from "react";
 
-// import FavoriteIconButton from "../FavoriteIconButton";
-// import IconButton from "../IconButton";
-// import Button from "../Button";
-// import { ThumbDown, ThumbUp } from "../SVGIcons";
+import FavoriteIconButton from "../FavoriteIconButton";
+import IconButton from "../IconButton";
+import Button from "../Button";
+import { ThumbDown, ThumbUp } from "../SVGIcons";
 
 import "./ItemCard.scss";
 
-// function Divider() {
-//   return <hr className="ItemCard__divider" />;
-// }
+function Divider() {
+  return <hr className="ItemCard__divider" />;
+}
 
-function ItemCard() {
-  // function onDownVote() {
-  //   handleDownVote(id);
-  // }
-  // function onUpVote() {
-  //   handleUpVote(id);
-  // }
-  // function onSetFavorite() {
-  //   handleSetFavorite(id);
-  // }
-  // function onAddToCart() {
-  //   handleAddToCart(id);
-  // }
+function ItemCard({
+    id, 
+    img, 
+    title, 
+    shortDescription,
+    isFavorite,
+    upVotes,
+    downVotes,
+    handleDownVote,
+    handleUpVote,
+    handleSetFavorite,
+    handleAddToCart
+  }) {
+  function onDownVote() {
+    handleDownVote(id);
+  }
+  function onUpVote() {
+    handleUpVote(id);
+  }
+  function onSetFavorite() {
+    handleSetFavorite(id);
+  }
+  function onAddToCart() {
+    handleAddToCart(id);
+  }
 
-  return <article className="ItemCard col col-12 col-md-6 col-lg-4" />;
+  return( 
+  <>
+    <article className="ItemCard col col-12 col-md-6 col-lg-4">
+      <div className="card mb-5">
+        <FavoriteIconButton handleSetFavorite={onSetFavorite} isFavorite />
+        <img className="img-fluid card-img-top" src={img} alt={title} />
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p className="card-text">{shortDescription}</p>
+          
+          <div className="d-flex justify-content-between align-items-center">
+            <div>
+              <IconButton aria-label="Up vote product" handleClick={onUpVote}> 
+                <ThumbDown />
+                <p>{downVotes.currentValue}</p>
+              </IconButton>
+              <IconButton aria-label="Down vote product" handleClick={onDownVote}> 
+                <ThumbUp />
+                <p>{upVotes.currentValue}</p>
+              </IconButton>
+            </div>
+            
+            <Button onClick={onAddToCart}> Add to cart </Button>
+            </div>
+          </div>
+        </div>
+    </article>
+  </>
+  );
 }
 
 export default ItemCard;
--- src/components/ProductsListing/ProductsListing.js
+++ src/components/ProductsListing/ProductsListing.js
@@ -1,13 +1,26 @@
 import React from "react";
 
-// import ItemCard from "../ItemCard";
+import ItemCard from "../ItemCard";
 
-function ProductsListing() {
+function ProductsListing({products, handleDownVote, handleUpVote, handleSetFavorite, handleAddToCart, ...props}) {
   return (
     <section className="row">
-      {/* {products.map((product) => (
-        <ItemCard ... />
-      ))} */}
+      {products.map((product) => (
+        <ItemCard 
+          key={product.id}
+          id={product.id}
+          img={product.img}
+          title={product.title}
+          shortDescription={product.shortDescription}
+          upVotes={product.votes.upVotes}
+          downVotes={product.votes.downVotes}
+          handleUpVote={handleUpVote}
+          handleDownVote={handleDownVote}
+          handleAddToCart={handleAddToCart}
+          handleSetFavorite={handleSetFavorite}
+          isFavorite={product.isFavorite}
+        />
+      ))}
     </section>
   );
 }
